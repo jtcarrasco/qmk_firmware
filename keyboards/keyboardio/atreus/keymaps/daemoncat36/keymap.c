582123f21b4f9f173ac16ac9f6df13179765d520
Jason Carrasco <info@jtcarrasco.com>
2025-05-14 23:31:38 -0700

    250514 formatting

--- keyboards/keyboardio/atreus/keymaps/daemoncat36/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/daemoncat36/keymap.c
@@ -35,21 +35,21 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWTY] = LAYOUT(
-    KC_Q,      KC_W,      KC_E,      KC_R,     KC_T,                           KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,
-    KC_A,      KC_S,      KC_D,      KC_F,     KC_G,                           KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,
-    MT_Z,      MT_X,      KC_C,      KC_V,     KC_B,     XXXXXXX,   XXXXXXX,   KC_N,     KC_M,     KC_COMM,  MT_RP,     MT_RS,
-    XXXXXXX,   XXXXXXX,   MT_ESC,    MO(_FUN), KC_LALT,  XXXXXXX,   XXXXXXX,   KC_SPC,   MO(_NUM), MT_ENT,   XXXXXXX,   XXXXXXX
+    KC_Q,     KC_W,      KC_E,    KC_R,      KC_T,                         KC_Y,    KC_U,      KC_I,     KC_O,     KC_P,
+    KC_A,     KC_S,      KC_D,    KC_F,      KC_G,                         KC_H,    KC_J,      KC_K,     KC_L,     KC_SCLN,
+    MT_Z,     MT_X,      KC_C,    KC_V,      KC_B,     XXXXXXX,  XXXXXXX,  KC_N,    KC_M,      KC_COMM,  MT_RP,    MT_RS,
+    XXXXXXX,  XXXXXXX,   MT_ESC,  MO(_FUN),  KC_LALT,  XXXXXXX,  XXXXXXX,  KC_SPC,  MO(_NUM),  MT_ENT,   XXXXXXX,  XXXXXXX
   ),
   [_NUM] = LAYOUT(
-    _______,   KC_1,      KC_2,   KC_3,      _______,                         KC_DEL,    _______,   _______,   _______,   KC_BSPC,
-    _______,   KC_4,      KC_5,   KC_6,      _______,                         KC_UP,     KC_LEFT,   KC_DOWN,   KC_RIGHT,  SC_RFH,
-    KC_LSFT,   KC_7,      KC_8,   KC_9,      _______,   XXXXXXX,   XXXXXXX,   KC_SAVE,   SC_CUT,    SC_COPY,   SC_PSTE,   KC_RSFT,
-    XXXXXXX,   XXXXXXX,   KC_0,   KC_TRNS,   KC_TRNS,   XXXXXXX,   XXXXXXX,   KC_TAB,    KC_TRNS,   KC_TRNS,   XXXXXXX,   XXXXXXX
+    _______,  KC_1,     KC_2,  KC_3,     _______,                      KC_DEL,   _______,  _______,  _______,   KC_BSPC,
+    _______,  KC_4,     KC_5,  KC_6,     _______,                      KC_UP,    KC_LEFT,  KC_DOWN,  KC_RIGHT,  SC_RFH,
+    KC_LSFT,  KC_7,     KC_8,  KC_9,     _______,  XXXXXXX,  XXXXXXX,  KC_SAVE,  SC_CUT,   SC_COPY,  SC_PSTE,   KC_RSFT,
+    XXXXXXX,  XXXXXXX,  KC_0,  KC_TRNS,  KC_TRNS,  XXXXXXX,  XXXXXXX,  KC_TAB,   KC_TRNS,  KC_TRNS,  XXXXXXX,   XXXXXXX
   ),
   [_FUN] = LAYOUT(
-    _______,   KC_F1,     KC_F2,    KC_F3,     KC_10,                           KC_NUBS,   KC_MINS,   KC_EQL,    KC_UNDS,   KC_PLUS,
-    _______,   KC_F4,     KC_F5,    KC_F6,     KC_11,                           KC_LCBR,   KC_RCBR,   KC_LBRC,   KC_RBRC,   KC_QUOT,
-    _______,   KC_F7,     KC_F8,    KC_F9,     KC_12,     XXXXXXX,   XXXXXXX,   KC_PIPE,   KC_LPRN,   KC_RPRN,   KC_BSLS,   _______,
-    XXXXXXX,   XXXXXXX,   KC_TRNS,  KC_TRNS,   KC_TRNS,   XXXXXXX,   XXXXXXX,   KC_TRNS,   KC_TRNS,   KC_TRNS,   XXXXXXX,   XXXXXXX
+    _______,  KC_F1,    KC_F2,    KC_F3,    KC_10,                        KC_NUBS,  KC_MINS,  KC_EQL,   KC_UNDS,  KC_PLUS,
+    _______,  KC_F4,    KC_F5,    KC_F6,    KC_11,                        KC_LCBR,  KC_RCBR,  KC_LBRC,  KC_RBRC,  KC_QUOT,
+    _______,  KC_F7,    KC_F8,    KC_F9,    KC_12,    XXXXXXX,  XXXXXXX,  KC_PIPE,  KC_LPRN,  KC_RPRN,  KC_BSLS,  _______,
+    XXXXXXX,  XXXXXXX,  KC_TRNS,  KC_TRNS,  KC_TRNS,  XXXXXXX,  XXXXXXX,  KC_TRNS,  KC_TRNS,  KC_TRNS,  XXXXXXX,  XXXXXXX
   )
 };
