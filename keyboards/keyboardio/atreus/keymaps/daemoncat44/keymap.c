d61710707fad2a8eb01c975d63c29a3a04b081bd
Jason Carrasco <info@jtcarrasco.com>
2025-09-03 16:38:25 -0700

    250903 updated

--- keyboards/keyboardio/atreus/keymaps/daemoncat44/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/daemoncat44/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWTY] = LAYOUT(
     KC_Q,     KC_W,      KC_E,    KC_R,      KC_T,                        KC_Y,    KC_U,      KC_I,     KC_O,    KC_P,
     KC_A,     KC_S,      KC_D,    KC_F,      KC_G,                        KC_H,    KC_J,      KC_K,     KC_L,    KC_SCLN,
-    MT_Z,     MT_X,      KC_C,    KC_V,      KC_B,     KC_HOME,  KC_PGUP, KC_N,    KC_M,      KC_COMM,  MT_RP,   MT_RS,
-    SC_V,     KC_F2,     KC_TAB,  MO(_FUN),  WIN_TAB,  KC_END,   KC_PGDN, KC_SPC,  MO(_NUM),  KC_ENT,   KC_F1,   MT_ESC
+    MT_Z,     MT_X,      KC_C,    KC_V,      KC_B,    KC_HOME,   KC_PGUP, KC_N,    KC_M,      KC_COMM,  MT_RP,   MT_RS,
+    SC_V,     KC_LCTL,   KC_LALT, MO(_FUN),  KC_TAB,  WIN_TAB,   KC_PGDN, KC_SPC,  MO(_NUM),  KC_ENT,   KC_RALT, MT_ESC
   ),
   [_NUM] = LAYOUT(
     _______,  KC_1,     KC_2,  KC_3,     _______,                      KC_DEL,   SC_AC,    SC_OUT,   SC_IN,   KC_BSPC,
