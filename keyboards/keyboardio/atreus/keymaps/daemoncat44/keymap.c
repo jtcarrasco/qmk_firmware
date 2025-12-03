193697fc8c61111f1efe05604c7bf4873e251aca
Jason Carrasco <info@jtcarrasco.com>
2025-12-03 00:53:44 -0800

    251203 update

--- keyboards/keyboardio/atreus/keymaps/daemoncat44/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/daemoncat44/keymap.c
@@ -37,13 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,     KC_W,      KC_E,    KC_R,      KC_T,                        KC_Y,    KC_U,      KC_I,     KC_O,    KC_P,
     KC_A,     KC_S,      KC_D,    KC_F,      KC_G,                        KC_H,    KC_J,      KC_K,     KC_L,    KC_SCLN,
     MT_Z,     MT_X,      KC_C,    KC_V,      KC_B,    KC_HOME,   KC_PGUP, KC_N,    KC_M,      KC_COMM,  MT_RP,   MT_RS,
-    SC_V,     KC_LCTL,   KC_LALT, MO(_FUN),  KC_TAB,  WIN_TAB,   KC_PGDN, KC_SPC,  MO(_NUM),  KC_ENT,   KC_RALT, MT_ESC
+    SC_V,     KC_LCTL,   KC_LALT, MO(_FUN),  KC_TAB,  KC_END,    KC_PGDN, KC_SPC,  MO(_NUM),  KC_ENT,   KC_RCTL, MT_ESC
   ),
   [_NUM] = LAYOUT(
-    _______,  KC_1,     KC_2,  KC_3,     _______,                      KC_DEL,   SC_AC,    SC_OUT,   SC_IN,   KC_BSPC,
+    _______,  KC_1,     KC_2,  KC_3,     KC_GRV,                       KC_DEL,   SC_AC,    SC_OUT,   SC_IN,   KC_BSPC,
     _______,  KC_4,     KC_5,  KC_6,     _______,                      KC_UP,    KC_LEFT,  KC_DOWN,  KC_RIGHT,  SC_RFH,
     KC_LSFT,  KC_7,     KC_8,  KC_9,     _______,  KC_TRNS,  KC_TRNS,  SC_SAVE,  SC_CUT,   SC_COPY,  SC_PSTE,   KC_RSFT,
-    KC_TRNS,  KC_TRNS,  KC_0,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TAB,   KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS
+    KC_TRNS,  KC_TRNS,  KC_0,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS
   ),
   [_FUN] = LAYOUT(
     KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F10,                       KC_NUBS,  KC_MINS,  KC_EQL,   KC_UNDS,  KC_PLUS,
